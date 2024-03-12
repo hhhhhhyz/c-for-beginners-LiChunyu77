e2d00020df3929f268e9ac2e6be1da949b480e0e
heyongzhe <1600191714@qq.com>
2024-03-12 22:28:27 +0800

    code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { ReactDOM } from 'react';
+
+import MultiSelect from './MultiSelect';
+
+const App = () => {
+  return (
+    <div>
+      <h1>MultiSelect</h1>
+      <MultiSelect />
+    </div>
+  );
+};
+
+export default App;
